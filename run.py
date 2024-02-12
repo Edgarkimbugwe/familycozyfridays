43545943eda93a37162311ef2d38bc410d183b0f
Edgarkimbugwe <>
2024-02-12 18:01:34 +0000

    Set up stucture to access data from the google spreadsheets

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# Open the Google Sheet by its name
+SHEET = GSPREAD_CLIENT.open('family_cozy_fridays')
+# Get the data from the sheet as a list of lists
+
+
+data = activity_scores.get_all_values()
+
+print(data)
