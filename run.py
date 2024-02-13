eb52c6beb18ad8855d5b38ccc43a3f606021e205
Edgarkimbugwe <>
2024-02-13 12:34:28 +0000

    added a function to update scores and to create a new column for a new player if that player is new to the group

--- run.py
+++ run.py
@@ -15,18 +15,39 @@ SHEET = GSPREAD_CLIENT.open('family_cozy_fridays')
 # Get the data from the sheet as a list of lists
 
 
-#function to add data in the activity_scores worksheet
+# function to add data in the activity_scores worksheet
 def add_activity(date, activity, players):
+    # Get the Activity_scores worksheet
     worksheet = SHEET.get_worksheet(0)
 
+    # Get the last activity ID from the worksheet
     last_activity_id = worksheet.cell(worksheet.row_count, 1).value
 
+    # generate unique ID for the activity
     activity_id = int(last_activity_id) + 1 if last_activity_id else 1
 
+    # Prepare data to append to the worksheet
     row_data = [activity_id, date, activity] + ['' for _ in range(len(players))]
-
+    
+    # Append the row to the worksheet
     worksheet.append_row(row_data)
 
 
-print(add_activity)
+# function to update scores for a specific activity
+def update_scores(activity_id, players, scores):
+    # Get the Activity_scores worksheet
+    worksheet = SHEET.get_worksheet(0)
+
+    try:
+        # Find the column index corresponding to the player
+        player_column = worksheet.find(player).col
+
+        # Update the score for the players in the specified activity row
+        worksheet.update_cell(activity_id + 1, player_column, score)
+    except gspread.exceptions.CellNotFound:
+        # If player not found, add a new column for the player
+        worksheet.add_cols(1)
+        player_column = worksheet.col_count
+        worksheet.update_cell(1, player_column, player)
+        worksheet.update_cell(activity_id + 1, player_column, score)
 
